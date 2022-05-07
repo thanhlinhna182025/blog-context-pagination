e6b99a0b8fe062eff18c755cc53e294a08bd1dd0
thanhtruong <thanhlinhna182025@gmail.com>
2022-05-07 09:25:31 +0700

    contain change

--- frontend/src/context/Contains.js
+++ frontend/src/context/Contains.js
@@ -1,6 +1,6 @@
 export const BASEURL =
   process.env.NODE_ENV !== "production"
-    ? "https://radiant-wave-38888.herokuapp.com"
-    : "http://localhost:4000";
+    ? "https://radiant-wave-38888.herokuapp.com/api"
+    : "http://localhost:4000/api";
 
 export const PF = "https://radiant-wave-38888.herokuapp.com/images/";
