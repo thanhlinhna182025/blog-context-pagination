3b0e028655ed1ee9d1af8f8b4e22a8fffc88ba0c
thanhtruong <thanhlinhna182025@gmail.com>
2022-05-07 08:29:46 +0700

    netlify

--- frontend/src/context/Contains.js
+++ frontend/src/context/Contains.js
@@ -1,2 +1,6 @@
-export const BASEURL = "http://localhost:4000";
-export const PF = "http://localhost:4000/images/";
+export const BASEURL =
+  process.env.NODE_ENV !== "production"
+    ? "https://radiant-wave-38888.herokuapp.com"
+    : "http://localhost:4000";
+
+export const PF = "https://radiant-wave-38888.herokuapp.com/images/";
